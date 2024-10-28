7b512420394a28ea4fa9c697f1175474c183c2cf
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-10-28 02:30:46 -0700

    JWT

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const JWT_SECRET = process.env.JWT_SECRET || 'default';
-// const { JWT_SECRET = 'dev-secret' } = process.env;
+// const JWT_SECRET = process.env.JWT_SECRET || 'default';
+const { JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = JWT_SECRET;
